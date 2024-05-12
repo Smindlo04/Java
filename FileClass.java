05c0dc39b7f91ee849132534a414c8a373ee9811
LM_Sibiya <116903062+Smindlo04@users.noreply.github.com>
2024-05-12 22:12:26 +0200

    Add files via upload

--- FileClass.java
+++ FileClass.java
@@ -1,58 +1,27 @@
-import java.io.*;
 import java.util.*;
-import javax.swing.*;
-
+import java.io.*;
 
 public class FileClass
 {
-    int counter = 0;
-    IBook []arrBooks = new IBook[25];
+    private int counter = 0;
     
+    ArrayList<Propertyable> arrProperty = new ArrayList<Propertyable>();
     
     public void readFromFile(){
         try{
-            Scanner input = new Scanner(new FileReader("books.txt"));
+            Scanner input = new Scanner(new FileReader("properties.txt"));
             
-            while(input.hasNextLine()){
+            while(input.hasNext()){
                 String line = input.nextLine();
-                String[] data = line.split("#");
-                String title = data[0];
-                String author = data[1];
-                String ISBN = data[2];
-                int NumOfPages;
-                double weight;
-                int fileSize;
-                
-                char ISBNChar = ISBN.charAt(0);
-                if(ISBNChar == '0')
-                {
-                
-                NumOfPages = Integer.parseInt(data[3]);
-                weight = Float.parseFloat(data[3] +1);
-                arrBooks[counter] = new PrintBook(title, author, ISBN, NumOfPages,weight);
-                counter++;
-                }
-                else
-                {
-                    
-                fileSize = Integer.parseInt(data[3]);
-                arrBooks[counter] = new EBook(title, author, ISBN, fileSize);
-                counter++;
-            }
-               
-                
+                String[] data = line.split(" ");
+                String ownerName;
+                String contactNum;
+                int duartion;
+                int size;
             }
         }
         catch(IOException e){
-            JOptionPane.showMessageDialog(null, "Error reading from file !!");
+            
         }
-    }
-    
-    public int getCount(){
-        return counter;
-    }
-    
-    public IBook[] getArray(){
-        return arrBooks;
     }
 }
